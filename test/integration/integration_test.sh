a1e2453ad43fa8c5cf91076df7abb8bb50beb616
Francisco Pitriqueo <franpitri@gmail.com>
2020-11-25 01:24:36 -0300

    🐛 fix: update env variable name

--- test/integration/integration_test.sh
+++ test/integration/integration_test.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-PLAYWRIGHT_CONFIG=$(echo '{"helpers": {"Playwright": {"url": "<URL>:5000"}}}' | sed 's/<URL>/'$VOTE_PUBLIC_IP'/g')
+PLAYWRIGHT_CONFIG=$(echo '{"helpers": {"Playwright": {"url": "http://<URL>:5000"}}}' | sed 's/<URL>/'$VOTE_PUBLIC_IP'/g')
 
-npx codeceptjs run --reporter mochawesome --override "$PUPPETEER_CONFIG" --steps
+npx codeceptjs run --reporter mochawesome --override "$PLAYWRIGHT_CONFIG" --steps
