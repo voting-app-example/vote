9fe5f9dd10619d52ba4326f9480bb2772ad3ad7b
Francisco Pitriqueo <franpitri@gmail.com>
2020-11-25 10:06:55 -0300

    📦 build: update deployment script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,10 +8,8 @@ TEMPLATE_FILE="$GITHUB_WORKSPACE/deploy/templates/vote-deployment.template.yaml"
 
 sed 's/<TAG>/'$TAG'/g' $TEMPLATE_FILE > $GITHUB_WORKSPACE/deploy/manifests/vote-deployment.yaml
 
-cat $GITHUB_WORKSPACE/deploy/manifests/vote-deployment.yaml
-
 KUBECONFIG_FILE="$GITHUB_WORKSPACE/deploy/kubeconfig.yml"
 
 kubectl --kubeconfig=$KUBECONFIG_FILE apply -f $GITHUB_WORKSPACE/deploy/manifests/
 
-#kubectl --kubeconfig=$KUBECONFIG_FILE rollout restart deployment/vote -n vote
+kubectl --kubeconfig=$KUBECONFIG_FILE rollout restart deployment/vote -n vote
